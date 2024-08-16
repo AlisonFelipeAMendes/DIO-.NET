0221dd2ac7bf9553eabf5b397fea66abc4962f84
Alison Mendes <alisonfelipeamendes@gmail.com>
2024-08-16 15:11:32 -0300

    Segundo commit - L

---
+++ Models/Pessoa.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SINTAXE_E_IDENTACAO.Models //Caminho lógico do projeto
+{
+    public class Pessoa
+    {   
+        public string? Nome { get; set; }
+        public int Idade { get; set; }
+        public string? NomeRepresentanteLegalDaPessoalFisica { get; set; }
+
+
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá meu nome é:{Nome}, e tenho {Idade} de idade.");
+
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using SINTAXE_E_IDENTACAO.Models;
+
+string apresentacao ="Seja bem vindo";
+int quantidade = 1;
+double altura = 1.80;
+
+
+
+//Pessoa p = new Pessoa();
+
+//p.Nome = "Alison";
+//p.Idade = 32;
+//p.Apresentar();
