fad0d4481121729448b700d1b237a5dfcfdd3dd8
Alison Mendes <alisonfelipeamendes@gmail.com>
2024-08-17 21:07:36 -0300

    Sintaxe Basica - Variaveis commit - L

--- obj/Debug/net8.0/SINTAXE_E_IDENTACAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/SINTAXE_E_IDENTACAO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SINTAXE_E_IDENTACAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85162f856aab08fe534ca0937f144b16e38b0eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d8ab47d818a466b983b320ee768ad37c864aa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SINTAXE_E_IDENTACAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SINTAXE_E_IDENTACAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
