4cf27dce832d492bce213bfebc6c6d8cad884524
Alison Mendes <alisonfelipeamendes@gmail.com>
2024-08-25 22:51:02 -0300

    Sintaxe - Operadores commit 02

---
+++ SINTAXE/OperadoresCondicionais.cs
@@ -0,0 +1,29 @@
+
+//Na Pratica
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possivel realizar a compra: {possivelVenda}");
+
+if(possivelVenda)
+{
+    Console.WriteLine("Venda Realizada!");
+}
+else{
+        Console.WriteLine("Desculpe. Não temos a quantidade!");
+
+}
+
+
+// if(quantidadeEmEstoque >= quantidadeCompra) 5+
+// {
+//     Console.WriteLine("Venda Realizada!");
+// }
+// else{
+//         Console.WriteLine("Desculpe. Não temos a quantidade!");
+
+// }
--- obj/Debug/net8.0/SINTAXE_E_IDENTACAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/SINTAXE_E_IDENTACAO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SINTAXE_E_IDENTACAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0575f68838aca5130549bc8f3cd9a2e753bc6a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9132eeeb40e2b9ef57d47cac40094b325603070e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SINTAXE_E_IDENTACAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SINTAXE_E_IDENTACAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
